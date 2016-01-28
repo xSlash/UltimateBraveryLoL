c40a8319192038eabb90625922a195b1ff79c242
Martin Kristensen <slash.development@gmail.com>
2016-01-28 22:24:12 +0100

    Initial commit

---
+++ app/src/main/java/slash/development/ultimatebraverylol/Second_Fragment.java
@@ -0,0 +1,22 @@
+package slash.development.ultimatebraverylol;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Martin on 27-01-2016.
+ */
+public class Second_Fragment extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
